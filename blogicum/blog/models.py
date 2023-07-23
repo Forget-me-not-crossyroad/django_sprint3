cbcb68dcb15b454624ea80798aeff9f5061fb66c
Forget-me-not-crossyroad <milleralex629@gmail.com>
2023-07-24 00:50:28 +0300

    Apply migrations, change attributes in 'blog.models'

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -11,13 +11,13 @@ class Category(PublishedModel):
     """Модель Категории."""
     title = models.CharField(
         max_length=256,
-        verbose_name='Название',
+        verbose_name='Заголовок',
         help_text='Название категории, не более 256 символов'
     )
     slug = models.SlugField(
         unique=True,
         max_length=64,
-        verbose_name='Слаг',
+        verbose_name='Идентификатор',
         help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.'
     )
     description = models.TextField(
@@ -26,7 +26,8 @@ class Category(PublishedModel):
     )
 
     class Meta:
-        verbose_name = 'объект "Категория"'
+        # verbose_name = 'объект "Категория"'
+        verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
     def __str__(self):
@@ -37,16 +38,17 @@ class Location(PublishedModel):
     """Модель Местоположения."""
     name = models.CharField(
         max_length=256,
-        verbose_name='Название',
-        help_text='Название местоположения, не более 256 символов',
+        verbose_name='Название места',
+        help_text='Название местоположения, не более 256 символов'
     )
 
     class Meta:
-        verbose_name = 'объект "Местоположение"',
+        # verbose_name = 'объект "Местоположение"',
+        verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class Post(PublishedModel):
@@ -54,39 +56,43 @@ class Post(PublishedModel):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор',
+        verbose_name='Автор публикации',
         help_text='Автор поста'
     )
     title = models.CharField(
         max_length=256,
-        verbose_name='Название',
-        help_text='Заголовок поста, не более 256 символов',
+        verbose_name='Заголовок',
+        help_text='Заголовок поста, не более 256 символов'
     )
     text = models.TextField(
         verbose_name='Текст',
         help_text='Текст поста'
     )
     pub_date = models.DateTimeField(
-        verbose_name='Дата',
-        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.',
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
+        null=True,
         related_name='post',
         verbose_name='Категория',
-        help_text='Название категории',
+        help_text='Название категории'
     )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
         related_name='post',
         verbose_name='Местоположение',
-        help_text='Название местоположения',
+        help_text='Название местоположения'
     )
 
     class Meta:
-        verbose_name = 'объект "Публикация"'
+        # verbose_name = 'объект "Публикация"'
+        verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
 
     def __str__(self):
