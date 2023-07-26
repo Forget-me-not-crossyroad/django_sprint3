7900441fa27370eaa97f3ee92dab62a2444525bd
Forget-me-not-crossyroad <milleralex629@gmail.com>
2023-07-27 02:53:57 +0300

    Apply grouping of apps to 'INSTALLED APPS' in '/blogicum/settings/'

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -10,15 +10,20 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
-    'blog.apps.BlogConfig',
-    'core.apps.CoreConfig',
+    # Приложения Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Локальные приложения
+    'pages.apps.PagesConfig',
+    'blog.apps.BlogConfig',
+    'core.apps.CoreConfig',
+
+    # Сторонние приложения
     'debug_toolbar',
 ]
 
